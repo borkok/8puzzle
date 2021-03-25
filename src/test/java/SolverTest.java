b269355c04420b1f0798b35a5220ce7c1b07ab65
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-03-25 20:02:21 +0100

    tests: more tests

--- src/test/java/SolverTest.java
+++ src/test/java/SolverTest.java
@@ -14,7 +14,7 @@ public class SolverTest {
     @Test
     public void init_exceptions() {
         assertThatThrownBy(() -> new Solver(null))
-            .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
@@ -35,35 +35,74 @@ public class SolverTest {
         return Stream.of(
                 Arguments.of(
                         //initial
-                        new Board(new int[][] {  {1,2},  {3,0}  }),
+                        new Board(new int[][] { { 1, 2 }, { 3, 0 } }),
                         //moves
                         0,
                         //solution
                         List.of(
-                                new Board(new int[][] {  {1,2},  {3,0}  })
+                                new Board(new int[][] { { 1, 2 }, { 3, 0 } })
                         )
                 )
-                ,Arguments.of(
+                , Arguments.of(
                         //initial
-                        new Board(new int[][] {  {1,2},  {0,3}  }),
+                        new Board(new int[][] { { 1, 2 }, { 0, 3 } }),
                         //moves
                         1,
                         //solution
                         List.of(
-                                new Board(new int[][] {  {1,2},  {3,0}  }),
-                                new Board(new int[][] {  {1,2},  {0,3}  })
+                                new Board(new int[][] { { 1, 2 }, { 3, 0 } }),
+                                new Board(new int[][] { { 1, 2 }, { 0, 3 } })
                         )
                 )
-                ,Arguments.of(
+                , Arguments.of(
                         //initial
-                        new Board(new int[][] {  {0,1},  {3,2}  }),
+                        new Board(new int[][] { { 0, 1 }, { 3, 2 } }),
                         //moves
                         2,
                         //solution
                         List.of(
-                                new Board(new int[][] {  {0,1},  {3,2}  }),
-                                new Board(new int[][] {  {1,0},  {3,2}  }),
-                                new Board(new int[][] {  {1,2},  {3,0}  })
+                                new Board(new int[][] { { 0, 1 }, { 3, 2 } }),
+                                new Board(new int[][] { { 1, 0 }, { 3, 2 } }),
+                                new Board(new int[][] { { 1, 2 }, { 3, 0 } })
+                        )
+                )
+                , Arguments.of(
+                        //initial
+                        new Board(new int[][] {
+                                { 0, 1, 3 },
+                                { 4, 2, 5 },
+                                { 7, 8, 6 }
+                        }),
+                        //moves
+                        4,
+                        //solution
+                        List.of(
+                                new Board(new int[][] {
+                                        { 0, 1, 3 },
+                                        { 4, 2, 5 },
+                                        { 7, 8, 6 }
+                                }),
+                                new Board(new int[][] {
+                                        { 1, 0, 3 },
+                                        { 4, 2, 5 },
+                                        { 7, 8, 6 }
+                                }),
+
+                                new Board(new int[][] {
+                                        { 1, 2, 3 },
+                                        { 4, 0, 5 },
+                                        { 7, 8, 6 }
+                                }),
+                                new Board(new int[][] {
+                                        { 1, 2, 3 },
+                                        { 4, 5, 0 },
+                                        { 7, 8, 6 }
+                                }),
+                                new Board(new int[][] {
+                                        { 1, 2, 3 },
+                                        { 4, 5, 6 },
+                                        { 7, 8, 0 }
+                                })
                         )
                 )
         );
@@ -84,8 +123,15 @@ public class SolverTest {
         return Stream.of(
                 Arguments.of(
                         //initial
-                        new Board(new int[][] {  {0,1},  {2,3}  })
+                        new Board(new int[][] { { 0, 1 }, { 2, 3 } })
                 )
+                , Arguments.of(
+                        //initial
+                        new Board(new int[][] {
+                                { 0, 1, 3 },
+                                { 4, 2, 5 },
+                                { 8, 7, 6 }
+                        }))
         );
     }
 }
